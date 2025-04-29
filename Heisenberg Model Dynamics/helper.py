76cb3ff97b5a919d5469a0cc8b32f3c6429cf33d
Pranavi Jain <pranviijain@gmail.com>
2025-04-28 17:11:15 -0700

    tfim update and xy model

--- Heisenberg Model Dynamics/helper.py
+++ Heisenberg Model Dynamics/helper.py
@@ -2,15 +2,17 @@ import pennylane as qml
 import numpy as np
 
 
-## Create the circuit to execute the time-evolution operator for a given time-step
+## Create the circuit to execute the time-evolution operator for TFIM for a given time-step
 
 def evolution_circuit_TFIM(num_time_steps, Jx, hz, freq, delta_t, N):
-    hbar = 0.658212  # eV*fs
+    hbar = 0.658212     # defined in units: eV*fs
+    
     # define rotation angles for gates in circuit
     psiX = -2.0 * Jx * delta_t / hbar
     for step in range(num_time_steps):
         t = (step + 0.5) * delta_t
         psiZ = -2.0 * hz * np.cos(2 * np.pi * freq * t) * delta_t / hbar
+    
         # implement XX operator
         for q in range(0, N - 1):
             qml.Hadamard(q)
@@ -20,6 +22,39 @@ def evolution_circuit_TFIM(num_time_steps, Jx, hz, freq, delta_t, N):
             qml.CNOT([q, q + 1])
             qml.Hadamard(q)
             qml.Hadamard(q + 1)
+    
         # implement Z operator
         for q in range(0, N):
             qml.RZ(psiZ, q)
+    return
+
+
+## Create the circuit to execute the time-evolution operator for XY model for a given time-step
+
+def evolution_circuit_XY(num_time_steps, Jx, Jy, delta_t, N):
+    hbar = 0.658212     # eV*fs
+    
+    # define rotation angles for gates in circuit
+    psiX = -2.0*Jx*delta_t/hbar
+    psiY = -2.0*Jy*delta_t/hbar
+
+    for step in range(num_time_steps):
+        # implement XX operator
+        for q in range(0, N - 1):
+            qml.Hadamard(q)
+            qml.Hadamard(q + 1)
+            qml.CNOT([q, q + 1])
+            qml.RZ(psiX, q + 1)
+            qml.CNOT([q, q + 1])
+            qml.Hadamard(q)
+            qml.Hadamard(q + 1)
+        # implement YY operator
+        for q in range(0, N-1):
+            qml.RX(-np.pi/2, q)
+            qml.RX(-np.pi/2,q+1)
+            qml.CNOT([q,q+1])
+            qml.RZ(psiY,q+1)
+            qml.CNOT([q,q+1])
+            qml.RX(np.pi/2, q)
+            qml.RX(np.pi/2,q+1)
+    return
---
+++ Heisenberg Model Dynamics/matchgates_tfim.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pennylane as qml
+
+
+## Creating circuit to implement matchgate for TFIM using PennyLane
+
+def make_matchgate(params, wire):
+    i = wire
+    theta_0, theta_1, theta_2, theta_3 = params
+    qml.RZ(theta_0, wires=i)
+    qml.RZ(theta_0, wires=i + 1)
+    qml.RX(np.pi/2, wires=i)
+    qml.RX(np.pi/2, wires=i + 1)
+    qml.CNOT(wires=[i, i + 1])
+    qml.RX(theta_1, wires=i)
+    qml.RZ(theta_2, wires=i + 1)
+    qml.CNOT(wires=[i, i + 1])
+    qml.RX(-np.pi/2, wires=i)
+    qml.RX(-np.pi/2, wires=i + 1)
+    qml.RZ(theta_3, wires=i)
+    qml.RZ(theta_3, wires=i + 1)
+    # matchgate = (RZ_layer * RX_layer * cnot * RXRZ_layer * cnot * RX_layer * RZ_layer)
+    return
+
+
+## Create circuit to implement a layer of matchgates on even qubits
+
+def make_layertype1(N, params):
+    for i in range(0, N - 1, 2):
+        make_matchgate(params, i)
+    return
+
+
+## Create circuit to implement a layer of matchgates on odd qubits
+
+def make_layertype2(N, params):
+    # N even
+    if N % 2 == 0:
+        for i in range(1, N - 1, 2):
+            make_matchgate(params, i)
+    # N odd
+    else:
+        for i in range(1, N, 2):
+            make_matchgate(params, i)
+    return
